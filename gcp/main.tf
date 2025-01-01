e6fe0cc2808602847211f32580d0de97a8fa67dc
uday singh <67567197+udaysingh007@users.noreply.github.com>
2025-01-01 20:29:12 +0000

    main.tf modified

--- gcp/main.tf
+++ gcp/main.tf
@@ -5,7 +5,7 @@ provider "google" {
   zone    = var.zone
 }
 
-# create vpc
+# create vpc for use by the VM
 resource "google_compute_network" "vpc" {
   name                    = "${var.project_name}-vpc"
   auto_create_subnetworks = false
